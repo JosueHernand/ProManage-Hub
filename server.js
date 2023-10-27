b66a77d6e90084d538fceab4426dabf9d3287e17
JosueHernand <joseh1337@gmail.com>
2023-10-27 01:10:28 -0700

    Configure middleware to parse JSON and url encoded data

--- server.js
+++ server.js
@@ -4,3 +4,6 @@ const connection = require('./config/connection');
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
