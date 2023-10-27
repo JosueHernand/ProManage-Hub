0c39fa149da5f5d8c5662dca0db0704725258c90
JosueHernand <joseh1337@gmail.com>
2023-10-27 01:11:07 -0700

    Started the express server and log a message when its running

--- server.js
+++ server.js
@@ -7,3 +7,5 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
