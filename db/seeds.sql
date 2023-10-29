ff82ae41361b53a389c9c714ad196c7fd571ef67
JosueHernand <joseh1337@gmail.com>
2023-10-28 19:04:40 -0700

    Inserted data into the role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,14 @@ VALUES ('Enginerring'),
     ('Finance'),
     ('Legal'),
     ('Sales');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 34344334, 4),
+    ("Salesperson", 50, 4),
+    ("Lead Engineer", 324, 1),
+    ("Software Engineer", 435, 1),
+    ("Account Manager", 324325, 2),
+    ("Accountant", 24525, 2),
+    ("Legal Team Lead", 324, 3),
+    ("Lawyer", 234234, 3),
+    ("Customer Service", 100000000, 4);
