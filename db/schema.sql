d392d9b027e1fb696c17d54b28c5e32d83cf4bd9
JosueHernand <joseh1337@gmail.com>
2023-10-28 19:03:00 -0700

    Created the employee table

--- db/schema.sql
+++ db/schema.sql
@@ -13,3 +13,11 @@ CREATE TABLE role (
     salary DECIMAL,
     department_id INT
 );
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);
