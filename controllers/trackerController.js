85b42e479ef8df0dd73cabe997bbad3f938c390d
JosueHernand <joseh1337@gmail.com>
2023-11-04 21:00:08 -0700

    Wrote a function to get the lis tof employees

--- controllers/trackerController.js
+++ controllers/trackerController.js
@@ -1,1 +1,14 @@
 const connection = require('../config/connection');
+
+const getEmployee = () => {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM employee', (error, results) => {
+        if (error) {
+            console.error(error);
+            reject(error);
+        } else {
+            resolve(results);
+        }
+        });
+    });
+};
