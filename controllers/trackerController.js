c536d6eb1a2709193a1dc70d610dbe60c3e0c26a
JosueHernand <joseh1337@gmail.com>
2023-11-04 21:03:23 -0700

    Wrote a function to be able to view all exmployees with their details

--- controllers/trackerController.js
+++ controllers/trackerController.js
@@ -62,3 +62,24 @@ const getRole = () => {
         });
     });
 }
+
+const viewAllEmployees = () => {
+    return new Promise((resolve, reject) => {
+        const query = `
+            SELECT e.id, e.first_name, e.last_name, r.title AS job_title, d.name AS department, r.salary, 
+            CONCAT(m.first_name, ' ', m.last_name) AS manager
+            FROM employee e
+            LEFT JOIN role r ON e.role_id = r.id
+            LEFT JOIN departments d ON r.department_id = d.id
+            LEFT JOIN employee m ON e.manager_id = m.id
+        `;
+        connection.query(query, (err, results) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            }else {
+                resolve(results);
+            }
+        });
+    });
+};
