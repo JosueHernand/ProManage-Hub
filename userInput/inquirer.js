ac8f736ac8a3cbeafb82b2bb1e85adc410859010
JosueHernand <joseh1337@gmail.com>
2023-11-05 16:49:58 -0800

    Defined main questions for user to choose from

--- userInput/inquirer.js
+++ userInput/inquirer.js
@@ -1,3 +1,19 @@
 const inquirer = require('inquirer');
 const connection = require('../config/connection');
 const { getRole, addDepartment, getDepartment, getEmployee, viewAllEmployees, roleDetails } = require('../controllers/trackerController');
+
+const mainQuestion = [
+    {
+        type: 'list',
+        name: 'questionList',
+        message: 'What would you like to do?',
+        choices: ['View All Employees',
+        'Add Employee', 
+        'Update Employee Role', 
+        'View All Roles', 
+        'Add Role', 
+        'View All Departments', 
+        'Add Department', 
+        'Quit'],
+    },
+];
